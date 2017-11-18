53c425cf913b1a1155cac7fe8b74e39a2da4da96
rawitP <rawit.p@ku.th>
2017-11-18 14:16:45 +0700

    Add counting person process

--- practicumPackage.py
+++ practicumPackage.py
@@ -1,3 +1,4 @@
+# !!! Already expect for usb.core.USBError
 from time import sleep
 import threading
 from practicum import *
@@ -7,21 +8,26 @@ CONNECT_MCU = [True, False] # CHECK, BABE
 RQ_READ = 1
 RQ_SET_LOCK = 2
 RQ_GET_BUFFER_DATA = 9
-BUFFER_DATA_LENGTH = 8
+BUFFER_DATA_LENGTH = 8 # Must be the same value in MCU Firmware
 RQ_READ_LENGTH = 5
-POLLING_INTERVAL = 0.5
+POLLING_INTERVAL = 0.3
 UNLOCK_RFID_DATA = ([213, 8, 171, 137, 255], [125, 52, 171, 169, 75])
 MCU_0_NAME = b'ID 5910500520'
 MCU_1_NAME = b'ID 5910500147'
 # Global variable  
 mcu_0 = None
-mcu_1 = None
-polling_data = None
+polling_data = None # Store raw data from Check's MCU
 is_polling = True
 rfid_data = [0, 0, 0, 0, 0]
-lock_status = 1 # 0: Unlock, 1: lock
-lock_button = 1
+lock_status = 0 # 0: Unlock, 1: lock
+lock_button = 1 # 0: Pressed, 1: Not Pressed
+counting_state = 0 # 0: Initial state, 1: Inside->Outside, 2: Outside->Inside
+                   # 3: Wait for button to not pressed
+outside_button = 1
+inside_button = 1
+counting_amount = 0
 # Babe Board
+mcu_1 = None
 temp_data = None # Int
 humid_data = None # Int
 # LED and Air status
@@ -48,12 +54,43 @@ def setup_usb():
         print('Not found Babe MCU')
         exit(1)
  
+def set_counting_state(val):
+    global counting_state
+    counting_state = val
+
+def change_counting_amount(val):
+    global counting_amount
+    counting_amount += val
+    print("Counting amount: %d" % counting_amount)
+
+def counting_process():
+    if counting_state == 3:
+        if inside_button == 0 or outside_button == 0:
+            pass
+        else:
+            set_counting_state(0)
+    if inside_button == 0:
+        if counting_state == 2:
+            change_counting_amount(1)
+            set_counting_state(3)
+        elif counting_state == 0:
+            set_counting_state(1)
+    elif outside_button == 0:
+        if counting_state == 1:
+            change_counting_amount(-1)
+            set_counting_state(3)
+        elif counting_state == 0:
+            set_counting_state(2)
+
 def set_lock(val):
     # TODO: Function that set servo degree
     mcu_0.usbWrite(RQ_SET_LOCK, index=7, value=val)
     global lock_status
     lock_status = val
     print("Lock status: %d" % val)
+
+def get_lock_status():
+    return lock_status
 
 def set_led(led_index, val):
     mcu_0.usbWrite(0, index=led_index, value=val)
@@ -70,18 +107,24 @@ def get_polling_data():
     return polling_data
 
 def polling_once():
-    data = mcu_0.usbRead(RQ_GET_BUFFER_DATA, length=BUFFER_DATA_LENGTH)
-    # Managing data from MCU
     global polling_data
-    polling_data = data
-    global rfid_data, lock_button
-    rfid_data = [polling_data[0], polling_data[1], polling_data[2], polling_data[3], polling_data[4]]
+    try:
+        data = mcu_0.usbRead(RQ_GET_BUFFER_DATA, length=BUFFER_DATA_LENGTH)
+        polling_data = data
+    except usb.core.USBError as err: ### !!! This is a very risky command !!! ###
+        print("USBError: {0}".format(err))
+        pass
+    #print(polling_data)
+    # Managing data from MCU
+    '''
+    ssrk;l
+    '''
+    global rfid_data, lock_button, outside_button, inside_button
+    rfid_data = [polling_data[0], polling_data[1], polling_data[2],
+                 polling_data[3], polling_data[4]]
     lock_button = polling_data[5]
-
-def polling_forever():
-    while(is_polling):
-        polling_once()
-        sleep(POLLING_INTERVAL)
+    outside_button = polling_data[6]
+    inside_button = polling_data[7]
 
 class mcuThread(threading.Thread) :
     def __init__(self):
@@ -99,7 +142,9 @@ class mcuThread(threading.Thread) :
                     break
         else:
             if lock_button == 0:
+                set_counting_state(0)
                 set_lock(1)
+            counting_process()  
 
     def setup(self):
         set_lock(lock_status)
@@ -107,7 +152,7 @@ class mcuThread(threading.Thread) :
     def run(self):
         print("Starting MCU Thread")
         self.setup()
-        while(self.is_running):
+        while self.is_running:
             polling_once()
             self.process()
             sleep(POLLING_INTERVAL)
@@ -125,7 +170,7 @@ if __name__ == "__main__":
     else:
         exit(1)
     # Prompt
-    while(True):
+    while True:
         # For error input, exit prompt
         try:
             val = int(input("Enter request: "))
@@ -136,8 +181,8 @@ if __name__ == "__main__":
             open_led()
         elif val == 3:
             close_led()
-        elif val == 1:
-            print(get_polling_data())   
+        elif val == 1:  
+            print(get_polling_data())
         elif val == 2:
             set_lock(1)
         elif val == -1:
